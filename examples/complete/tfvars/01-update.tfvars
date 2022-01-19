39e49e55a35d09d315198a11d9ad693051acf2e7
liuyuan <709503583@qq.com>
2022-01-19 15:45:05 +0800

    Improves the module examples/complete

---
+++ examples/complete/tfvars/01-update.tfvars
@@ -0,0 +1,9 @@
+#alicloud_ons_instance
+name        = "update-tf-ons-redis"
+description = "update-tf-ons-redis-description"
+
+#alicloud_kvstore_instance
+security_ips                 = ["10.23.12.24"]
+redis_engine_version         = "5.0"
+redis_appendonly             = "no"
+redis_lazyfree_lazy_eviction = "no"
--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -2,28 +2,16 @@
 variable "name" {
   description = "The specification of module name."
   type        = string
-  default     = ""
+  default     = "tf-ons-redis"
 }
 
 variable "description" {
   description = "The specification of module description."
   type        = string
-  default     = ""
+  default     = "tf-ons-redis-description"
 }
 
 #alicloud_kvstore_instance
-variable "vswitch_id" {
-  description = "VSwitch variables, if vswitch_id is empty, then the net_type = classic."
-  type        = string
-  default     = ""
-}
-
-variable "availability_zone" {
-  description = "The available zone to launch modules."
-  type        = string
-  default     = ""
-}
-
 variable "security_ips" {
   description = "The specification of the security ips."
   type        = list(string)
@@ -46,22 +34,4 @@ variable "redis_lazyfree_lazy_eviction" {
   description = "The specification of the redis lazyfree-lazy-eviction."
   type        = string
   default     = "yes"
-}
-
-variable "redis_resource_group_id" {
-  description = "The specification of the redis resource group id."
-  type        = string
-  default     = ""
-}
-
-variable "redis_instance_class" {
-  description = "The specification of the redis resource instance class."
-  type        = string
-  default     = "redis.master.large.default"
-}
-
-variable "security_group_ids" {
-  description = "A list of security group ids to associate with."
-  type        = list(string)
-  default     = []
 }
